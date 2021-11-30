36d60d2ef99722349bd7ab378fd1b13b45de38f4
Markus <markus.liang@gmail.com>
2021-11-30 22:50:40 +0000

    remove csv path hard coded

--- src/main/java/exercise/Main.java
+++ src/main/java/exercise/Main.java
@@ -2,23 +2,47 @@ package exercise;
 
 import addressbook.*;
 import csv.*;
+import java.io.File;
 import java.util.List;
 import person.Person;
 
 
 public class Main {
+    public static String getCsvPath(){
+        String csvPath = "";
+        try
+        {
+            csvPath = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+            csvPath = csvPath.substring(0, csvPath.lastIndexOf("/")) + "/testdata/address-book.csv";
+            System.out.println("csvPath = " + csvPath);
+
+            // released path            
+            File f = new File(csvPath);
+            if (!f.exists()) {
+                csvPath = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+                // debug path
+                csvPath = csvPath.substring(0, csvPath.lastIndexOf("/"));
+                csvPath = csvPath.substring(0, csvPath.lastIndexOf("/"));
+                csvPath += "/testdata/address-book.csv";
+            }
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return csvPath;
+    }
+    
     public static void main(String[] args) {
-        String csvPath = "/Users/maku_tachi/Downloads/coding-challenge-csv-master/src/testdata/address-book.csv";
+        String csvPath = getCsvPath();
         ICsvHandler csvHandler = new CsvHandler(csvPath);        
         boolean isLoaded = csvHandler.load();
         if(isLoaded){
             List<List<String>> listPerson = csvHandler.getData();
             IAddressBook addressBook = new AddressBook();
             addressBook.importRecords(listPerson);
-            
+
             List<Person> femaleRecords = addressBook.filterDataByGender(Person.Gender.Female);
             System.out.println("Number of females inside address book: " + femaleRecords.size());
-            
+
             addressBook.sortByAgeDesc();
             List<Person> sortedData = addressBook.getData();
             Person oldestPerson = sortedData.get(0);
@@ -26,7 +50,7 @@ public class Main {
         }else{
             System.out.println("Failed to load csv.");
             System.out.println(csvHandler.getlastError());
-        }        
+        }
     }
 
 }
